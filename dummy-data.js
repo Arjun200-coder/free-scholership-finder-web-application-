7fecf2fb8447f8207e01a2d64a2af3902e4f3c0e
Arjun <weareallforyou12345@gmail.com>
2024-12-23 19:30:28 +0530

    Add files via upload
    added all the files.

---
+++ dummy-data.js
@@ -0,0 +1,81 @@
+const scholarships = [
+    {
+      name: "Pre-Matric Scholarship",
+      eligibility: "Class 8, Class 9-10, income = 1 lakh (SC), 1.5 lakh (OBC) ",
+      benefits: "Tuition, books, educational expenses",
+      website: "https://scholarship.up.gov.in/",
+    },
+    {
+      name: "Pre-Matric Scholarship",
+      eligibility: "Class 9-10, Class 11-12, income = 1.5 lakh (OBC/SC), 2 lakh (Minority/OBC/Gen) ",
+      benefits: "Tuition, books, educational expenses",
+      website: "https://scholarship.up.gov.in/",
+    },
+    {
+      name: "Post-Matric Scholarship",
+      eligibility: "Class 11-12, income = 2 lakh (Gen/OBC), 2.5 lakh (SC/ST), 1.5 lakh (SC/ST/Minority)",
+      benefits: "Tuition, academic expenses, maintenance",
+      website: "https://scholarship.up.gov.in/",
+    },
+    {
+      name: "Post-Matric Scholarship",
+      eligibility: "UG, PG, Diploma, income = 1.5 lakh (Gen/OBC), 2 lakh (SC), 2.5 lakh (ST/Minority)",
+      benefits: "Tuition, academic expenses, maintenance",
+      website: "https://scholarship.up.gov.in/",
+    },
+    {
+      name: "Dashmottar Scholarship",
+      eligibility: "UG, PG, Ph.D, income = 1.5 lakh (Gen/OBC), 2 lakh (SC/ST)",
+      benefits: "Academic fees, living expenses",
+      website: "https://scholarship.up.gov.in/",
+    },
+    {
+      name: "NMMSS (National Means-cum-Merit Scholarship)",
+      eligibility: "Class 8, income = 1.5 lakh (Gen/OBC), 2.5 lakh (SC/ST)",
+      benefits: "Rs. 12,000 per annum",
+      website: "https://scholarships.gov.in/",
+    },
+    {
+      name: "State Merit Scholarship",
+      eligibility: "Class 11-12, UG, income = 1.5 lakh (Gen/OBC), 2 lakh (SC/ST) ",
+      benefits: "Tuition and academic fees",
+      website: "https://statemerit.up.gov.in/",
+    },
+    {
+      name: "Minority Scholarship",
+      eligibility: " Class 8, Class 9-10, income = 1 Lakh (Minority/OBC)",
+      benefits: "Tuition, Maintenance",
+      website: "https://scholarship.up.gov.in/",
+    },
+    {
+      name: "Minority Scholarship",
+      eligibility: " Ph.D , PG, income = 2 Lakh (Minority/SC/ST), income ≤ 2 Lakh (Gen/OBC)",
+      benefits: "Tuition, Maintenance",
+      website: "https://scholarship.up.gov.in/",
+    },
+    {
+      name: "Scholarship for Disabled Students",
+      eligibility: "UG, Ph.D ,PG, income = 2.5 Lakh (Minority), 1.5 Lakh (OBC), 1 Lakh (Gen)",
+      benefits: "Tuition, books, equipment",
+      website: "https://scholarship.up.gov.in/",
+    },
+    {
+      name: "Chief Minister's Fellowship",
+      eligibility: " Ph.D, PG, Minority, income = 2 Lakh (ST/SC), 1.5 Lakh (Minority/OBC)",
+      benefits: "Monthly Stipend, Tuition Fee waiver",
+      website: "https://chiefministerfellowship.up.gov.in/",
+    },
+    {
+      name: "Eklavya Scholarship",
+      eligibility: "Class 9-10, class 8 ,income = 1 Lakh (Minority/OBC), 1.5 Lakh (SC/ST)",
+      benefits: "Financial Assistance for higher education",
+      website: "https://scholarship.up.gov.in/",
+    },
+    {
+      name: "Women Empowerment Scholarship",
+      eligibility: " Ph.D,UG, PG, income = 2 lakh (OBC), 2.5 Lakh (Minority/SC), 1.5 Lakh (Gen)",
+      benefits: "Full tuition waiver, stipend for books",
+      website: "https://womenempowerment.up.gov.in/",
+    },
+  ];
+  
